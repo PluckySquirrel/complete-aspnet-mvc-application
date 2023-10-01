7ee2c7e107eb09585e7cc3a042ef393ee19ad538
Luong Anh <hoanganhluong2@gmail.com>
2023-10-01 17:02:08 +0700

    Completed EntityBaseRepository

--- eTickets/Data/Services/ActorsService.cs
+++ eTickets/Data/Services/ActorsService.cs
@@ -24,23 +24,23 @@ namespace eTickets.Data.Services
             await _context.SaveChangesAsync();
 		}
 
-        public async Task<IEnumerable<Actor>> GetAllAsync()
-        {
-            var result = await _context.Actors.ToListAsync();
-            return result;
-        }
-
-        public async Task<Actor> GetByIdAsync(int id)
-        {
-            var result = await _context.Actors.FirstOrDefaultAsync(n => n.Id == id);
-            return result;
-        }
-
-        public async Task<Actor> UpdateAsync(int id, Actor newActor)
+		public async Task<Actor> UpdateAsync(int id, Actor newActor)
         {
             _context.Update(newActor);
             await _context.SaveChangesAsync();
             return newActor;
         }
-    }
+
+		public async Task<IEnumerable<Actor>> GetAllAsync()
+		{
+			var result = await _context.Actors.ToListAsync();
+			return result;
+		}
+
+		public async Task<Actor> GetByIdAsync(int id)
+		{
+			var result = await _context.Actors.FirstOrDefaultAsync(n => n.Id == id);
+			return result;
+		}
+	}
 }
