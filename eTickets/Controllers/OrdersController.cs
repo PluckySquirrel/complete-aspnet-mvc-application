5e184d43116518a5366ad6ac2d86f143ed8b8749
Luong Anh <hoanganhluong2@gmail.com>
2023-10-10 22:27:29 +0700

    Added add item and remove items from cart

--- eTickets/Controllers/OrdersController.cs
+++ eTickets/Controllers/OrdersController.cs
@@ -28,5 +28,29 @@ namespace eTickets.Controllers
 
 			return View(response);
 		}
+
+		public async Task<IActionResult> AddItemToShoppingCart(int id)
+		{
+			var item = await _movieService.GetMovieByIdAsync(id);
+
+			if(item != null)
+			{
+				_shoppingCart.AddItemToCart(item);
+
+			}
+			return RedirectToAction(nameof(ShoppingCart));
+		}
+
+		public async Task<IActionResult> RemoveItemFromShoppingCart(int id)
+		{
+			var item = await _movieService.GetMovieByIdAsync(id);
+
+			if (item != null)
+			{
+				_shoppingCart.RemoveItemFromCart(item);
+
+			}
+			return RedirectToAction(nameof(ShoppingCart));
+		}
 	}
 }
