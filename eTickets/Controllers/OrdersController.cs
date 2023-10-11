05d1fd66c303e52b0e1984cd0c6eadf681c81ac8
Luong Anh <hoanganhluong2@gmail.com>
2023-10-11 16:03:20 +0700

    Added Listing all orders and ordering items without paypal functions

--- eTickets/Controllers/OrdersController.cs
+++ eTickets/Controllers/OrdersController.cs
@@ -9,12 +9,21 @@ namespace eTickets.Controllers
 	{
 		private readonly IMovieService _movieService;
 		private readonly ShoppingCart _shoppingCart;
+		private readonly IOrdersService _ordersService;
 
-        public OrdersController(IMovieService movieService, ShoppingCart shoppingCart)
+        public OrdersController(IMovieService movieService, ShoppingCart shoppingCart, IOrdersService ordersService)
         {
             _movieService = movieService;
 			_shoppingCart = shoppingCart;
+			_ordersService = ordersService;
         }
+
+		public async Task<IActionResult> Index()
+		{
+			string userId = "";
+			var orders = await _ordersService.GetOrdersByUserIdAsync(userId);
+			return View(orders);
+		}
         public IActionResult ShoppingCart()
 		{
 			var items = _shoppingCart.GetShoppingCartItems();
@@ -51,6 +60,18 @@ namespace eTickets.Controllers
 
 			}
 			return RedirectToAction(nameof(ShoppingCart));
+		}
+
+		public async Task<IActionResult> CompleteOrder()
+		{
+			var items = _shoppingCart.GetShoppingCartItems();
+			string userId = "";
+			string userEmailAddress = "";
+
+			await _ordersService.StoreOrderAsync(items, userId, userEmailAddress);
+			await _shoppingCart.ClearShoppingCartAsync();
+
+			return View("OrderCompleted");
 		}
 	}
 }
--- eTickets/Data/Services/IOrdersService.cs
+++ eTickets/Data/Services/IOrdersService.cs
@@ -4,7 +4,7 @@ namespace eTickets.Data.Services
 {
 	public interface IOrdersService
 	{
-		Task StoreOrder(List<ShoppingCartItem> items, string userId, string userEmailAddress);
+		Task StoreOrderAsync(List<ShoppingCartItem> items, string userId, string userEmailAddress);
 		Task<List<Order>> GetOrdersByUserIdAsync(string userId);
 
 	}
